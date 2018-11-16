913bb26be35ffeab61383209da8de3397d7091f1
Reuben Ayres <ra@sent.com>
2018-11-16 15:51:09 -0600

    registration route works

--- controllers/UserAPIController.rb
+++ controllers/UserAPIController.rb
@@ -2,19 +2,39 @@ class UserAPIController < ApplicationController
 
   # login POST '/api/user/login'
 
+
   # register POST '/api/user/register'
   post '/register' do
     # receive JSON requests like this
     payload_body = request.body.read
     payload = JSON.parse(payload_body).symbolize_keys
 
-    {
-      status: 200,
-      message: "hit register route",
-      payload: payload
-    }.to_json
+    # user should not be able to choose a username that is taken
+    user_exists = User.find_by username: payload[:username]
 
-  end
+    if user_exists
+      {
+        status: 421, # there may be a better status code here
+        message: "Username already taken."
+      }.to_json
+    else
+      user = User.new
+      user.username = payload[:username]
+      user.password = payload[:password] # note this will automatically encrypt
+                                        # bc we said has_secure_password in the User model
+      user.save
+      
+      session[:logged_in] = true
+      session[:username] = user.username
+
+      {
+        status: 200,
+        message: "Successfully registered user #{user.username}",
+      }.to_json
+
+    end
+
+  end # post '/register' do
 
   # logout GET '/api/user/logout'
 
